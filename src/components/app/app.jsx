ef9f616552bc291a85338ade280c4140a88b8548
Shuric <shuric110@yandex.ru>
2020-10-05 17:03:17 +0300

    Основные компоненты, роутинг

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,40 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+
 import WelcomeScreen from "../welcome-screen/welcome-screen";
+import AuthScreen from "../auth-screen/auth-screen";
+import LoseScreen from "../lose-screen/lose-screen";
+import QuestionArtistScreen from "../question-artist-screen/question-artist-screen";
+import QuestionGenreScreen from "../question-genre-screen/question-genre-screen";
+import WinScreen from "../win-screen/win-screen";
 
 const App = (props) => {
   const {errorCount} = props;
 
   return (
-    <WelcomeScreen errorCount={errorCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen errorCount={errorCount} />
+        </Route>
+        <Route exact path="/login">
+          <AuthScreen />
+        </Route>
+        <Route exact path="/result">
+          <WinScreen />
+        </Route>
+        <Route exact path="/lose">
+          <LoseScreen />
+        </Route>
+        <Route exact path="/dev-artist">
+          <QuestionArtistScreen />
+        </Route>
+        <Route exact path="/dev-genre">
+          <QuestionGenreScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
