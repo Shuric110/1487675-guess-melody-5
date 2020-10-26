e5b17effd8d301dcc49ec2968db102991bd15628
Shuric <shuric110@yandex.ru>
2020-10-26 13:59:44 +0300

    Подключение redux; вынесение состояния приложения в хранилище

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
 import App from "./components/app/app";
+import {reducer} from "./store/reducer";
 
 import questions from "./mocks/questions";
 
@@ -8,10 +12,17 @@ const Settings = {
   ERRORS_COUNT: 3
 };
 
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
+
 ReactDOM.render(
-    <App
-      errorCount={Settings.ERRORS_COUNT}
-      questions={questions}
-    />,
+    <Provider store={store}>
+      <App
+        errorCount={Settings.ERRORS_COUNT}
+        questions={questions}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
